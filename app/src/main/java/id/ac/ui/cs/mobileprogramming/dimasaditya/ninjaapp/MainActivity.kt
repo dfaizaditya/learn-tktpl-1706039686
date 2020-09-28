7ce04b4ab9a17f85b53aada0bc91f03ab4ca351b
DimasAditya <dimas.aditya73@ui.ac.id>
2020-09-28 19:42:44 +0700

    Finished lab-02

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/dimasaditya/ninjaapp/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/dimasaditya/ninjaapp/MainActivity.kt
@@ -1,17 +1,41 @@
 package id.ac.ui.cs.mobileprogramming.dimasaditya.ninjaapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import android.view.View
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : Activity() {
+
+    private val TAG = "MainActivity"
+
+    private lateinit var nameText: EditText
+
+    private lateinit var emailText: EditText
+
+    private var emailValidation = EmailValidation()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // display Toast here...
-        Toast.makeText(this,"Hello world",Toast.LENGTH_SHORT).show()
+        nameText = findViewById(R.id.userNameInput)
+        emailText = findViewById(R.id.emailInput)
 
+        emailText.addTextChangedListener(emailValidation)
+
+    }
+
+    fun onSaveClick(@Suppress("UNUSED_PARAMETER") view: View) {
+        if (!emailValidation.isValid) {
+            emailText.error = "Invalid email"
+            Log.w(TAG, "This email is invalid")
+            return
+        }
+
+        val name = nameText.text.toString()
+        val email = emailText.text.toString()
     }
 }
